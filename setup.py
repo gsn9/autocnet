13b666e60b410003121f7ea16d8d1c830460bbe0
jlaura <jlaura@users.noreply.github.com>
2021-08-27 12:40:00 +0000

    Bump version to 0.6.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 with open('README.md', 'r') as f:
     long_description = f.read()
 
-__version__ = '0.6.1'
+__version__ = '0.6.2'
 
 def setup_package():
     setup(
